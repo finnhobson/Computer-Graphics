8fdc958ac9e7573eb7f04b4c3ee72b6144417875
Finn Hobson <finn@hobsonmail.com>
2019-02-28 18:17:22 +0000

    Draws Vertices of Triangles

--- rasteriser/Source/skeleton.cpp
+++ rasteriser/Source/skeleton.cpp
@@ -6,6 +6,8 @@
 #include <stdint.h>
 
 using namespace std;
+using glm::vec2;
+using glm::ivec2;
 using glm::vec3;
 using glm::mat3;
 using glm::vec4;
@@ -13,20 +15,32 @@ using glm::mat4;
 
 SDL_Event event;
 
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 256
+#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 500
 #define FULLSCREEN_MODE false
+
+
+/* ----------------------------------------------------------------------------*/
+/* GLOBAL VARIABLES                                                            */
+vector<Triangle> triangles;
+
+vec4 cameraPos( 0, 0, -3.001, 1 );
+
 
 /* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 bool Update();
 void Draw(screen* screen);
+void VertexShader( const vec4& v, ivec2& p );
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result );
 
 int main( int argc, char* argv[] )
 {
-  
+
   screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
+
+  LoadTestModel(triangles);
 
   while ( Update())
     {
@@ -45,14 +59,20 @@ void Draw(screen* screen)
 {
   /* Clear buffer */
   memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
-    {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
+
+  for( uint32_t i=0; i<triangles.size(); ++i ){
+    vector<vec4> vertices(3);
+    vertices[0] = triangles[i].v0;
+    vertices[1] = triangles[i].v1;
+    vertices[2] = triangles[i].v2;
+
+    for(int v=0; v<3; ++v) {
+      ivec2 projPos;
+      VertexShader( vertices[v], projPos );
+      vec3 color(1,1,1);
+      PutPixelSDL( screen, projPos.x, projPos.y, color );
     }
+  }
 }
 
 /*Place updates of parameters here*/
@@ -63,6 +83,7 @@ bool Update()
   int t2 = SDL_GetTicks();
   float dt = float(t2-t);
   t = t2;
+  std::cout << "Render time: " << dt << " ms." << std::endl;
 
   SDL_Event e;
   while(SDL_PollEvent(&e))
@@ -93,7 +114,31 @@ bool Update()
 		/* Move camera quit */
 		return false;
 	      }
-	  }  
+	  }
     }
   return true;
+}
+
+
+void VertexShader( const vec4& v, ivec2& projPos ) {
+  float bigX = v.x - cameraPos.x;
+  float bigY = v.y - cameraPos.y;
+  float bigZ = v.z - cameraPos.z;
+
+  projPos.x = (SCREEN_HEIGHT * bigX / bigZ) + (SCREEN_WIDTH/2);
+  projPos.y = (SCREEN_HEIGHT * bigY / bigZ) + (SCREEN_HEIGHT/2);
+
+}
+
+
+void Interpolate( ivec2 a, ivec2 b, vector<ivec2>& result )
+{
+  int N = result.size();
+  vec2 step = vec2(b-a) / float(max(N-1,1));
+  vec2 current( a );
+  for( int i=0; i<N; ++i )
+  {
+    result[i] = current;
+    current += step;
+  }
 }
